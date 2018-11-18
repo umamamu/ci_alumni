b6bcdbce9cecce90601723926b1eac96f741a100
umamamu <khoirul.marjianto@gmail.com>
2018-11-18 18:02:17 +0800

    tahap 3

---
+++ application/controllers/Dashboard.php
@@ -0,0 +1,7 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+	class Dashboard extends CI_Controller {
+		public function index()
+		{
+		echo '<h1>Ini fungsi index dari controller dashboard</h1>';
+		}
+}
